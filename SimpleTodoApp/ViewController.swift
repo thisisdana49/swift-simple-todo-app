011979e1f3c666cd2fba5eba41c793381025f349
thisisdana49 <thisisdana49@gmail.com>
2024-03-26 19:09:34 +0900

    Initial Commit

---
+++ SimpleTodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTodoApp
+//
+//  Created by 조다은 on 3/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
